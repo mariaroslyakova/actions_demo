ee83fbe457479c81e418edf0f126a29019b8de7a
mariaroslyakova <165595568+mariaroslyakova@users.noreply.github.com>
2025-12-03 21:23:38 +0300

    Update test2.py

--- tests/test2.py
+++ tests/test2.py
@@ -1,4 +1,8 @@
-from src.taskClass import EBook
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from src.taskClass import Book
 
 
 def test_ebook_download():
